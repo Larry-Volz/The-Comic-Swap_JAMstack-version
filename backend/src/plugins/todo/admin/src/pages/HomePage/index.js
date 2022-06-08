8abbc50f3c7eeca96e25a0246a6c4b5dd5db77f7
Larry Volz <imaginologist@gmail.com>
2022-06-08 01:48:35 -0400

    basic TodoModal working

--- backend/src/plugins/todo/admin/src/pages/HomePage/index.js
+++ backend/src/plugins/todo/admin/src/pages/HomePage/index.js
@@ -1,55 +1,51 @@
-/*
- *
- * HomePage
- *
- */
-
 import React, { memo, useState } from "react";
-
+import { nanoid } from "nanoid";
 import { BaseHeaderLayout, ContentLayout } from "@strapi/design-system/Layout";
-
 import { EmptyStateLayout } from "@strapi/design-system/EmptyStateLayout";
 import { Illo } from "../../components/Illo";
 import { Button } from "@strapi/design-system/Button";
 import Plus from "@strapi/icons/Plus";
+import TodoModal from "../../components/TodoModal";
 
 // import PropTypes from 'prop-types';
 
-
-
 const HomePage = () => {
   const [todoData, setTodoData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  async function addTodo(data) {
+    setTodoData([...todoData, { ...data, id: nanoid(), isDone: false }]);
+  }
 
   return (
     <>
-      
       <BaseHeaderLayout
-        title="Larry's Home made Todo Plugin"
-        subtitle="Keepin' it organized"
-        as='h2'
+        title="Todo Plugin"
+        subtitle="All your todos in one place."
+        as="h2"
       />
 
       <ContentLayout>
         {todoData.length === 0 ? (
           <EmptyStateLayout
-          icon={<Illo />}
-          content="You don't have any todos yet..."
-          action={
-            <Button
-              onClick={() => setShowModal(true)}
-              variant="secondary"
-              startIcon={<Plus />}
-            >
-              Add your first todo
-            </Button>
-          }
-        />
-        ):(
-          "you have some"
-          // add count and table componennet here
+            icon={<Illo />}
+            content="You don't have any todos yet..."
+            action={
+              <Button
+                onClick={() => setShowModal(true)}
+                variant="secondary"
+                startIcon={<Plus />}
+              >
+                Add your first todo
+              </Button>
+            }
+          />
+        ) : (
+          <h2>You have {todoData.length} count</h2>
         )}
       </ContentLayout>
-      
+
+      {showModal && <TodoModal setShowModal={setShowModal} addTodo={addTodo} />}
     </>
   );
 };
