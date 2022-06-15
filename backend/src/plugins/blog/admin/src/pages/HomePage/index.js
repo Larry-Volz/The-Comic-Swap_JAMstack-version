3b968439194c4225dd4023ffcf91d36881d393e1
Larry Volz <imaginologist@gmail.com>
2022-06-14 20:33:49 -0400

    added basic layout component for blog

--- backend/src/plugins/blog/admin/src/pages/HomePage/index.js
+++ backend/src/plugins/blog/admin/src/pages/HomePage/index.js
@@ -4,15 +4,44 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, { memo, useState } from "react";
+
 // import PropTypes from 'prop-types';
 import pluginId from '../../pluginId';
 
+import { Layout } from '@strapi/design-system/Layout';
+import {ContentLayout, BaseHeaderLayout } from '@strapi/design-system/Layout';
+
+
 const HomePage = () => {
+  const [blogData, setBlogData] = useState([]);
+
   return (
     <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+
+      <Layout>
+
+        <BaseHeaderLayout
+          title="Blog Plugin"
+          subtitle ="What's new in YOUR universe?"
+          as="h2"
+        />
+
+        <ContentLayout>
+          <p>The Official Comic Swap Blog</p>
+        </ContentLayout>
+
+        {blogData.length === 0  ? 
+          (
+            <p>add emptystate component here</p>
+          ) : ( 
+            <p>add count and table component here</p>
+         )
+        }
+
+
+      </Layout>
+
     </div>
   );
 };
